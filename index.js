549ecd4f63dffa4d0daa9b74d957ef8f9404a9a5
faisalahmed7 <faisalahmeddiu@gmail.comm>
2022-05-07 16:13:03 +0600

    created Database in MONGODB and .env file implemented

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 
 
@@ -9,6 +10,17 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.n44g7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+
+    const collection = client.db("test").collection("devices");
+    console.log('connected with MONGODB');
+    // perform actions on the collection object
+    client.close();
+});
 
 
 app.get('/', (req, res) => {

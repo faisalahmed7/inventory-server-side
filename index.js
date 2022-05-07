4de5feefb88707ebd82b8af4363ec97509eda231
faisalahmed7 <faisalahmeddiu@gmail.comm>
2022-05-07 16:49:40 +0600

    data load from product collection

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const res = require('express/lib/response');
 require('dotenv').config()
 
 
@@ -14,13 +15,23 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.n44g7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
+async function run() {
 
-    const collection = client.db("test").collection("devices");
-    console.log('connected with DBMONGO');
-    // perform actions on the collection object
-    client.close();
-});
+    try {
+        await client.connect();
+        const productCollection = client.db('electronicsProducts').collection('product');
+        app.get('/inventory', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query)
+            const products = await cursor.toArray()
+            res.send(products)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
 
 
 app.get('/', (req, res) => {

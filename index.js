18f934def7d84a10ec6c8176cc36ede5c0bad087
faisalahmed7 <faisalahmeddiu@gmail.comm>
2022-05-08 12:57:19 +0600

    dynamic id created and displayed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const res = require('express/lib/response');
 require('dotenv').config()
 
@@ -26,6 +26,13 @@ async function run() {
             const products = await cursor.toArray()
             res.send(products)
         })
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const product = await productCollection.findOne(query)
+            res.send(product)
+        })
+
     }
     finally {
 

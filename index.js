a0a40fce89d4bde4b0254c230c11a25d5dff7a26
faisalahmed7 <faisalahmeddiu@gmail.comm>
2022-05-08 15:58:51 +0600

    POST Method implemention added for new Product

--- index.js
+++ index.js
@@ -33,6 +33,12 @@ async function run() {
             res.send(product)
         })
 
+        app.post('/inventory', async (req, res) => {
+            const newProduct = req.body;
+            const result = await productCollection.insertOne(newProduct)
+            res.send(result)
+        })
+
     }
     finally {
 
